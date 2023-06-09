2a0f606a8588c659b1448e6b192ce5fa5df96088
Elorabaekenechukwu <elorabaekenechukwu@gmail.com>
2023-06-09 03:30:25 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,7 +6,7 @@
   * @n: the inputed value
   * Return: the value of the bit at index index or -1 if an error occured
   */
-int get_bit(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
