ce630989f90f798153333dc06b230190479c02ae
Elorabaekenechukwu <elorabaekenechukwu@gmail.com>
2023-08-31 01:18:28 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * get_endianness - a fxn that checks the endianness
+  * Return: 0 if big endian, 1 if little endian
+  */
+int get_endianness(void)
+{
+	unsigned int j;
+	char *a;
+
+	j = 1;
+	a = (char *) &j;
+
+	return ((int)*a);
+}
+
